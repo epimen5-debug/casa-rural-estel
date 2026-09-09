569afe9318c7b6faf0f5375ef95de4cb92087a28
epimen5-debug <epimen5@gmail.com>
2026-09-09 12:03:58 +0200

    Update Navbar.tsx

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -47,7 +47,7 @@ export default function Navbar() {
             className="flex items-center gap-3 group"
           >
             <img
-              src="/client/public/Logo.png"
+              src="/Logo.png"
               alt="Casa Rural Estel"
               className="h-10 w-10 object-contain"
             />
